0a06f145fcc550c333c7e2f8d3fb24d3cb383d94
Nathanael <nathanael.ama1@gmail.com>
2018-01-05 14:49:35 +0200

    Intial Login page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,28 +3,36 @@ import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
+import { HttpModule } from '@angular/http';
+import { HttpClientModule } from '@angular/common/http'; 
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
+import {LoginPage} from '../pages/login/login';
+import { Auth } from '../providers/auth/auth';
 
 @NgModule({
   declarations: [
     MyApp,
-    HomePage
+    HomePage,
+    LoginPage
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp),
+    HttpClientModule
   ],
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    HomePage
+    HomePage,
+    LoginPage
   ],
   providers: [
     StatusBar,
     SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
+    {provide: ErrorHandler, useClass: IonicErrorHandler},
+    Auth
   ]
 })
 export class AppModule {}
---
+++ src/providers/auth/auth.ts
@@ -0,0 +1,28 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+/*
+  Generated class for the AuthProvider provider.
+
+  See https://angular.io/guide/dependency-injection for more info on providers
+  and Angular DI.
+*/
+@Injectable()
+export class Auth {
+
+  constructor(public http: HttpClient) {
+    console.log('Hello AuthProvider Provider');
+  }
+
+  login(){
+
+  	return new Promise((resolve) => {
+
+  		setTimeout(()=> {
+  			resolve(false);
+  		}, 3000);
+  	
+  	});
+  }
+
+}
